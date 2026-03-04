10a07a7b67343a94cca9d2f73994d1edce46fb8b
Elpah <obengelpachris@gmail.com>
2026-03-04 03:20:29 +0100

    setup: added lib/utils

---
+++ src/lib/utils.ts
@@ -0,0 +1,17 @@
+export function ensureLightMode() {
+  if (typeof document !== 'undefined') {
+    document.documentElement.classList.toggle('dark', false);
+  }
+}
+
+/**
+ * Removes any dark mode classes from a className string
+ * @param className The class string to process
+ * @returns The class string with dark mode classes removed
+ */
+export function removeDarkClasses(className: string): string {
+  return className
+    .split(' ')
+    .filter(cls => !cls.startsWith('dark:'))
+    .join(' ');
+}
