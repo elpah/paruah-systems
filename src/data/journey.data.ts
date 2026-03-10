7516cef5ec1613fb6601969ea22bbc21e96e7f04
Elpah <obengelpachris@gmail.com>
2026-03-10 04:43:12 +0100

    chore: added some data

---
+++ src/data/journey.data.ts
@@ -0,0 +1,32 @@
+export const JOURNEY = [
+  {
+    id: '01',
+    title: 'Consultation',
+    desc: 'Understanding your goals and requirements.',
+  },
+  {
+    id: '02',
+    title: 'Architecture',
+    desc: 'Planning the system structure and technology.',
+  },
+  {
+    id: '03',
+    title: 'Design',
+    desc: 'Designing intuitive user interfaces and experiences.',
+  },
+  {
+    id: '04',
+    title: 'Development',
+    desc: 'Building the platform with modern technologies.',
+  },
+  {
+    id: '05',
+    title: 'Testing',
+    desc: 'Ensuring reliability, performance, and security.',
+  },
+  {
+    id: '06',
+    title: 'Deployment',
+    desc: 'Launching and preparing the system to scale.',
+  },
+];
---
+++ src/data/recentWorks.data.ts
@@ -0,0 +1,24 @@
+export const RECENT_WORKS = [
+  {
+    title: 'ELPA Travel Consult',
+    category: 'Travel Agency',
+    description:
+      'A modern website designed for a travel and tour agency to showcase destinations, services, and attract new clients.',
+    image: '/images/travelAgency.png',
+  },
+  {
+    title: 'ICGC Living Word Temple',
+    category: 'Church Website',
+    description:
+      'Designed for a church to share sermons, announce events, and connect with its community online.',
+    image: '/images/icgclwt.png',
+  },
+  {
+    title: 'El-Gate Properties',
+    category: 'Real Estate',
+    description:
+      'A modern website designed for a real estate company to showcase properties, highlight services, and capture client inquiries.',
+    image:
+      'https://images.unsplash.com/photo-1586528116311-ad8dd3c8310d?q=80&w=1200&auto=format&fit=crop',
+  },
+];
